c01d5a9537ff785d0348a8f0407f0db54afc3c72
pyragnis <enzosabbatorsi@outlook.fr>
2024-09-04 11:03:42 +0200

    test add

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const stripe = require('stripe')('sk_test_51OexNZKqnFl9CSHJPDuPtianNXkdUT437GLzUSxbaar29WHs0D71EGOy4zTkUc6b279qQtjxtZ4whV5hAlYHvIjy001TGLtULG');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3010;
+
+app.use(cors()); // Autorisez toutes les requêtes CORS
+
+app.use(bodyParser.json());
+
+// Point de terminaison pour créer une session de paiement
+app.post('/create-checkout-session', async (req, res) => {
+  const { items, success_url, cancel_url } = req.body;
+
+  // Créer une session de paiement avec les informations fournies
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: items.map(item => ({
+        price_data: {
+          currency: 'eur',
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: item.amount,
+        },
+        quantity: 1,
+      })),
+      mode: 'payment',
+      success_url,
+      cancel_url,
+    });
+
+    // Renvoyer l'ID de session au client
+    res.json({ sessionId: session.url });
+  } catch (error) {
+    console.error('Erreur lors de la création de la session de paiement :', error);
+    res.status(500).json({ error: 'Erreur lors de la création de la session de paiement' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
