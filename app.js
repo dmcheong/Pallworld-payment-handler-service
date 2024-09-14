e573b38336627eb1e6e5cec04e5bda3acd2ea146
Precious <precious.soriano@ynov.com>
2024-09-14 02:27:29 +0200

    fix: to match purchase history

--- app.js
+++ app.js
@@ -2,47 +2,65 @@ const express = require('express');
 const stripe = require('stripe')('sk_test_51OexNZKqnFl9CSHJPDuPtianNXkdUT437GLzUSxbaar29WHs0D71EGOy4zTkUc6b279qQtjxtZ4whV5hAlYHvIjy001TGLtULG');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 const port = 3010;
 
-app.use(cors()); // Autorisez toutes les requêtes CORS
+app.use(cors());
 app.use(bodyParser.json());
 
-// Point de terminaison pour créer une session de paiement
 app.post('/create-checkout-session', async (req, res) => {
-  const { items, success_url, cancel_url } = req.body;
+    const { items, userId, name, street, city, postalCode, success_url, cancel_url } = req.body;
 
-  // Affiche les éléments reçus
-  console.log('Items reçus pour Stripe:', items);
+    try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: items.map(item => ({
+                price_data: {
+                    currency: 'eur',
+                    product_data: {
+                        name: item.name,
+                    },
+                    unit_amount: item.amount,
+                },
+                quantity: item.quantity,
+            })),
+            mode: 'payment',
+            success_url,
+            cancel_url,
+        });
 
-  try {
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: items.map(item => ({
-        price_data: {
-          currency: 'eur',
-          product_data: {
-            name: item.name,
-            description: item.description
-          },
-          unit_amount: item.amount,
-        },
-        quantity: item.quantity,
-      })),
-      mode: 'payment',
-      success_url,
-      cancel_url,
-    });
+        const orderData = {
+            userId,
+            items: items.map(item => ({
+                productId: item.productId,
+                name: item.name,
+                quantity: item.quantity,
+                price: item.amount / 100,
+                color: item.color || 'Couleur non définie',
+                size: item.size || 'Taille non définie',
+                customizationOptions: item.customizationOptions || {},
+            })),
+            totalAmount: items.reduce((total, item) => total + item.amount * item.quantity, 0) / 100,
+            shippingAddress: {
+                name,
+                street,
+                city,
+                postalCode,
+            }
+        };
 
-    res.json({ sessionId: session.url });
-  } catch (error) {
-    console.error('Erreur lors de la création de la session de paiement :', error);
-    res.status(500).json({ error: 'Erreur lors de la création de la session de paiement' });
-  }
+        await axios.post('http://localhost:3005/api/orders', orderData);
+        console.log('Données envoyées à l\'API:', orderData);
+
+        res.json({ sessionId: session.url });
+    } catch (error) {
+        console.error('Erreur lors de la création de la session de paiement :', error);
+        res.status(500).json({ error: 'Erreur lors de la création de la session de paiement' });
+    }
 });
 
-
 app.listen(port, () => {
-  console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+  console.log(`Serveur Express en cours d'exécution sur le port ${port}`);
 });
